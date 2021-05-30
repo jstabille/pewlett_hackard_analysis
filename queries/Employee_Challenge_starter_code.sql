c3f5dfb9d9eb5c05d66e57836849c2313c42bdac
Jeferson Stabille <78064878+jstabille@users.noreply.github.com>
2021-05-30 12:52:11 -0400

    Add files via upload

---
+++ queries/Employee_Challenge_starter_code.sql
@@ -0,0 +1,45 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+
+
+
+-- Unique retirement list
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, ti.to_date DESC;
+
+
+-- NOT unique list
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, ti.from_date DESC;
+
+
+-- COUNT by title of retiring list
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT DESC;	
+
+SELECT * FROM retiring_titles
+
+
